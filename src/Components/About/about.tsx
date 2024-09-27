d602b356f3f5c06fcad1e4b1388bec02b241ff41
vanesabucher <buchervanesa@gmail.com>
2024-09-26 22:37:49 -0300

    l

--- src/Components/About/about.tsx
+++ src/Components/About/about.tsx
@@ -1,7 +1,7 @@
 const About = () => {
   return (
       <div className="py-20">
-          <section className="text-center max-w-5xl mx-auto px-4">
+          <section className="text-center max-w-5xl mx-auto px-6 md:px-10 lg:px-12">
               <h2 className="text-4xl md:text-5xl font-bold text-white mb-8 tracking-wide">Sobre mí</h2>
               <p className="text-base md:text-lg text-gray-400 leading-relaxed mb-6">
                   Soy Full Stack Developer con una especialización en Front-End, y cuento con experiencia en la creación de páginas web diseñadas para potenciar la venta de productos y servicios. Manejo tecnologías como Next.js, React, Tailwind, CSS, y también tengo experiencia en backend utilizando Express, TypeORM, SQL Shell, JavaScript y TypeScript. <br /> 
